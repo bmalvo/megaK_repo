369944040e9697b747fbe990082141ac2d807687
bmalvo <boydmalvo@gmail.com>
2024-01-23 13:10:53 +0100

    hamburger menu

---
+++ menu-hamburger/main.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.querySelector('.container__hamburger');
+const menu = document.querySelector('.container__menu');
+
+
+hamburgerButton.addEventListener('click', () => {
+    menu.classList.toggle('open');
+    hamburgerButton.classList.toggle('open');
+})
