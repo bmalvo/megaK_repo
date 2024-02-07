abf158ee93dd97cf799af035afb10919f67bde8f
bmalvo <boydmalvo@gmail.com>
2024-02-07 18:47:11 +0100

    webpack_plugin, dev_server, mapy_zrodlowe

--- react/webpack/webpack.config.js
+++ react/webpack/webpack.config.js
@@ -1,6 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
@@ -11,7 +13,25 @@ module.exports = {
     rules: [{
       test: /\.css$/i,
       use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.js$/i,
+      exclude: /node_modules/,
+      use: 'babel-loader',
     }],
-  },  
-  mode: 'none',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    hot: true,
+  },
+  mode: 'development',
 };
