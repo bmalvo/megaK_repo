a7e7288798223687bc352e51f60bea05481aa61d
bmalvo <boydmalvo@gmail.com>
2024-02-29 20:44:59 +0100

    create check-sum

--- node/exercises/exercise_project/decrypt.js
+++ node/exercises/exercise_project/decrypt.js
@@ -1,6 +1,6 @@
 const { readFile, writeFile } = require('fs').promises;
-const { decryptText } = require('C:/Users/boydm/dev/megaK_repo/node/cipher/cipher');
-const { SALT } = require('./constants');
+const { decryptText, hash } = require('../C:/Users/boydm/dev/megaK_repo/node/cipher/cipher');
+const { SALT, HASH_SALT } = require('./constants');
 
 const [, , fileName, pwd] = process.argv;
 
@@ -8,5 +8,11 @@ const [, , fileName, pwd] = process.argv;
   const json = await readFile(fileName, 'utf8');
   const encrypted = JSON.parse(json);
   const decrypted = await decryptText(encrypted.encrypted, pwd, SALT, encrypted.iv);
-  await writeFile(fileName, decrypted, 'utf8');
+  const decryptedHash = hash(decrypted, HASH_SALT);
+
+  if (decryptedHash === encrypted.hash) {
+    await writeFile(fileName, decrypted, 'utf8');
+  } else {
+    console.error('File is not original');
+  }
 })();
--- node/exercises/exercise_project/encrypt.js
+++ node/exercises/exercise_project/encrypt.js
@@ -1,13 +1,15 @@
 const { readFile, writeFile } = require('fs').promises;
-const { encryptText } = require('C:/Users/boydm/dev/megaK_repo/node/cipher/cipher');
-const { SALT } = require('./constants');
+const { encryptText, hash } = require('C:/Users/boydm/dev/megaK_repo/node/cipher/cipher');
+const { SALT, HASH_SALT } = require('./constants');
 
 // const SALT = 'abcABC123!@#';
 const [, , fileName, pwd] = process.argv;
 
 (async () => {
   const content = await readFile(fileName, 'utf8');
+  const contentHash = hash(content, HASH_SALT);
   const encrypted = await encryptText(content, pwd, SALT);
+  encrypted.hash = contentHash;
   await writeFile(fileName, JSON.stringify(encrypted), 'utf8');
   console.log('Done');
 })();
