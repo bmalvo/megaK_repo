9ec8f9c3f4d450df1b3b5d95647ad64b5add1e8c
bmalvo <boydmalvo@gmail.com>
2024-03-04 11:22:08 +0100

    atrybut porównania key, zadanie domowe

--- react/my-vite-app/src/main.tsx
+++ react/my-vite-app/src/main.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import './index.css'
+
+const objArr = [
+  {
+    id: 1,
+    color: 'red',
+    size: 40
+  },
+  {
+    id: 2,
+    color: 'blue',
+    size: 60
+  },
+  {
+    id: 3,
+    color: 'green',
+    size: 80
+},
+]
+
+const text = 'Lorem ipsum';
+
+const lorem = text.split('')
+
+const ifEven = (text: string, num: number) => {
+  if (num % 2 === 0) {
+    return text.toUpperCase()
+  } else {
+    return text.toLocaleLowerCase()
+  }
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <div>
+      {objArr.map(el => <div key={el.id} style={{ backgroundColor: el.color, width: el.size, height: el.size }}>{el.color}</div>)}
+      <ul>
+        {lorem.map((letter, index) => <li key={index}>{ ifEven(letter,index) }</li>)}
+      </ul>
+    </div>
   </React.StrictMode>,
 )
