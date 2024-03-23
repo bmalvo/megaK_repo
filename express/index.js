29004ab3a42b6d45b876c08c6e5225f84ba451b0
bmalvo <boydmalvo@gmail.com>
2024-03-23 11:31:38 +0100

    object request

--- express/index.js
+++ express/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const { URLSearchParams } = require('url');
+
+function generateQueryString(params) {
+  const qs = new URLSearchParams(params);
+  return `${qs}`.replace(/\+/g, '%20');
+}
+
+console.log(`http://localhost:3000/?${generateQueryString({
+  name: 'Bartek & Kuba',
+})}`);
 
 const app = express();
 
-app.get('/', () => {
+app.get('/', (req) => {
+  console.log(req.hostname);
+  console.log(req.ip);
+  console.log(req.method);
+  console.log(req.url);
+  console.log(req.originalUrl);
+  console.log(req.path);
+  console.log(req.protocol);
+  console.log(req.secure);
   console.log('Display books list');
 });
 
